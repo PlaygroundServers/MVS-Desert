dc9ff4df072c276a18067390e3ccd84748ed3109
tluth001 <tony@luthmedia.com>
2021-08-25 15:56:27 -0500

    Updating core

--- MVS-Desert/config.cpp
+++ MVS-Desert/config.cpp
@@ -24,5 +24,38 @@ class CfgMods
 };
 class cfgVehicles
 {
-
+	class MVS_Canteen_Base;
+	class MVS_Cap_Base;
+	class MVS_Helmet_Base;
+	class BoonieHat_Olive;
+	class MVS_OpsCore_Base;
+	class MVS_ArmoredHelmet_Base;
+	class MVS_WarriorHelmet_Base;
+	class MVS_CombatShirt_Base;
+	class MVS_CombatPants_Base;
+	class TacticalShirt_Olive;
+	class GorkaEJacket_Flat;
+	class GorkaPants_Flat;
+	class ModularChestRig_Base;
+	class ModularVestSystem_Base;
+	class PlateCarrierHolster;
+	class Modular_Pouch_Base;
+	class Modular_Pouch_Base_Heavy;
+	class Modular_Belt_Base;
+	class MVS_Sheath_Base;
+	class MVS_Compact_Base;
+	class MVS_Compact_Base_2;
+	class MVS_Assault_Pack_Tan;
+	class MVS_Pack_Base;
+	class MVS_Rucksack_Base;
+	class MVS_Molle_Panel_Base;
+	class Modular_Pouch_Base_Utility;
+	class Modular_Pouch_Base_Medical;
+	class Modular_Pouch_Base_Taco;
+	class MVS_Balaclava2;
+	class MVS_Balaclava3;
+	class MVS_Shroud_Base;
+	class MVS_S10Respirator;
+	class MVS_PMK2Respirator;
+	class MVS_M50Respirator;
 };
