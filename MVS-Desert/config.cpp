d9987d7e7925bf8fa75175076c5968529ee6cf01
tluth001 <tony@luthmedia.com>
2021-08-28 20:30:05 -0500

    Fixing Compile Classes

--- MVS-Desert/config.cpp
+++ MVS-Desert/config.cpp
@@ -37,7 +37,6 @@ class cfgVehicles
 	class Modular_Belt_Base;
 	class MVS_Sheath_Base;
 	class MVS_Compact_Base;
-	class MVS_Sling_Pack_Base;
 	class Modular_Pouch_Base_Taco;
 	class MVS_Sling_Pack_Base;
 	class MVS_Pack_Base;
