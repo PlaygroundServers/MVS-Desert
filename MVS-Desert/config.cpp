1eb93f9c106ad3d1854f00757db54f9d0d78c21c
tluth001 <tony@luthmedia.com>
2021-08-28 20:43:33 -0500

    Fixing Compile Classes

--- MVS-Desert/config.cpp
+++ MVS-Desert/config.cpp
@@ -251,21 +251,21 @@ class cfgVehicles
 		scope = 2;
 		displayName = "MVS Heavy LMG Pouch - Desert Digital";
 		heatIsolation = 0.8;
-		hiddenSelectionsTextures[] = {"\WinterCamoForModularVestSystem\Data\Digisnow\MVSHeavy_co_DesertDigi.paa"};
+		hiddenSelectionsTextures[] = {"\MVS-Desert\data\Digisnow\MVSHeavy_co_DesertDigi.paa"};
 	};
 	class MVS_Heavy_Pouch_Desert_Multicam: Modular_Pouch_Base_Heavy
 	{
 		scope = 2;
 		displayName = "MVS Heavy LMG Pouch - Desert Multicam";
 		heatIsolation = 0.8;
-		hiddenSelectionsTextures[] = {"\WinterCamoForModularVestSystem\Data\MVSHeavy_co_MC_Desert.paa"};
+		hiddenSelectionsTextures[] = {"\MVS-Desert\data\MVSHeavy_co_MC_Desert.paa"};
 	};
 	class MVS_Heavy_Pouch_Desert: Modular_Pouch_Base_Heavy
 	{
 		scope = 2;
 		displayName = "MVS Heavy LMG Pouch - Desert";
 		heatIsolation = 0.8;
-		hiddenSelectionsTextures[] = {"\WinterCamoForModularVestSystem\Data\MVSHeavy_co_Desert.paa"};
+		hiddenSelectionsTextures[] = {"\MVS-Desert\data\MVSHeavy_co_Desert.paa"};
 	};
 	class MVS_Warrior_Desert_Digi: MVS_WarriorHelmet_Base
 	{
